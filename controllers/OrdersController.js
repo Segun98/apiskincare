cb7e32825dbbeca83c6316a6e4647fe747ecdb27
Segun <shegunolanitori@gmail.com>
2021-01-31 19:19:40 +0100

    Feat: fixed email content to best list orders

--- controllers/OrdersController.js
+++ controllers/OrdersController.js
@@ -6,31 +6,33 @@ sgMail.setApiKey(process.env.SEND_GRID_KEY);
 async function OrderToVendor(req, res) {
     const {
         to,
-        body,
+        order,
         orderId
     } = req.body
-
-
 
     try {
         const vendor = await knex("users").select("business_name").where({
             email: to
         })
 
-        const orders = await knex("orders").select("name").where({
-            order_id: orderId
-        })
+        // const orders = await knex("orders").select("name").where({
+        //     order_id: orderId
+        // })
 
         const content = {
             to: "shegunolanitori@gmail.com",
             from: "orders@tadlace.com",
             subject: `New Order on Tadlace. Order ID: ${orderId}`,
             html: `<body><p>Dear ${vendor[0].business_name || vendor},</p>
-            <p>You have a new order! A dispatch rider will contact you soon! Please always ensure your product is in good condition and is always readily available.</p>
+            <p>You have a new order! Track it in your <a href="${host[0]}/vendor/orders">orders page</a></p>
+            
+            <p>A dispatch rider will contact you soon! Please always ensure your product is in good condition and is always readily available.</p>
+
             <ul>
-            <li>Product: ${orders[0].name}</li>
+            <li>Product: ${order.name} - Quantity: ${order.quantity}</li>
             </ul>
-            <p>Remember to check your public store page to ensure your physical products in stock corresponds with the number online</p>
+
+            <p>Remember to check your public store page to ensure your available physical products in stock corresponds with the number online</p>
             </body>`
         }
 
@@ -45,30 +47,31 @@ async function OrderToVendor(req, res) {
 async function OrderToCustomer(req, res) {
     const {
         to,
-        body,
+        order,
         name,
         orderId
     } = req.body
 
 
+    let products = ""
+    for (let i = 0; i < order.length; i++) {
+        products += `<li style="font-weight:bold" >${order[i].name} x ${order[i].quantity}</li> `
+    }
 
     try {
-        const orders = await knex("orders").select("name").where({
-            order_id: orderId
-        })
-
         const content = {
             to: "shegunolanitori@gmail.com",
             from: "orders@tadlace.com",
-            subject: `Your Order has been successfuly placed! Order ID: ${orderId}`,
+            subject: `Successful Order! Order ID: ${orderId}`,
             html: `<body><p>Hello ${name},</p>
-            <p>Your Order: ${orders[0].name} has been successfuly placed!</p>
-            <ul>
-            <li>Track your Orders in your <a href="${host[0]}/customer/orders">orders page</a></li>
-            <li>Orders are usually delivered within 2-4 days from order date</li>
-            <li><a href="${host[0]}/customer#contact"> Contact us</a> if there is any issue</li>
-            <li>Happy shopping!</li>
-            </ul>
+            <p>Your Order has been successfuly placed!</p>
+            <ol>
+            ${products}
+            </ol>
+            <p>Track your Orders in your <a href="${host[0]}/customer/orders">orders page</a></p>
+            <p>Orders are usually delivered within 2-4 days from order date</p>
+            <p><a href="${host[0]}/customer#contact"> Contact us</a> if there is any issue</p>
+            <p>Happy shopping!</p>
             <p></p>
             <p>Thank you</p>
             </body>`
