f2a01a54312cbd4f4fea12f57401964fda3b2530
Segun98 <shegunolanitori@gmail.com>
2021-05-14 23:44:25 +0100

    Feat: added dot com domain

--- bin/environment.js
+++ bin/environment.js
@@ -1,6 +1,6 @@
 //endpoints
 let local = ['http://localhost:3000', 'http://localhost:5000']
-let prod = ['https://tadlace.vercel.app', 'https://adminpartystore.vercel.app', 'https://tadlace.com']
+let prod = ['https://tadlace.com', 'https://adminpartystore.vercel.app']
 
 
 module.exports = process.env.NODE_ENV === "production" ? prod : local
