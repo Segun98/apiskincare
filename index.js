100919a9208ccf0769584e8976bfb64d2ac1042e
Segun98 <shegunolanitori@gmail.com>
2021-05-19 21:29:14 +0100

    Feat: limit IP to 50 requests per ms

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use(cors({
 //rate limiter
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100 // limit each IP to 100 requests per windowMs
+    max: 50 // limit each IP to 100 requests per windowMs
 });
 
 // //  apply to all requests
